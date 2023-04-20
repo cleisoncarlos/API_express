a005fb3cf72e92ff3ac293582f8cc28ddaf99a51
Cleison Carlos <cleisonspider@yahoo.com.br>
2023-04-20 17:45:15 -0300

    Autenticação de rotas usando JWT

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express')
 const app = express()
 
+const jwt = require('jsonwebtoken')
+
+const JWTsecret = 'sadabsodubasdds5d14as65d1as65d1sa65d1sa65d1sa6d5sa1'
+
+
+
+
+
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+
+
+
+//middleware-----------------
+
+function auth(req, res, next) {
+  const authToken = req.headers['authorization']
+  console.log(authToken)
+
+  next();
+
+}
+
+//--------------------------
+
+
+
+
+
+
 
 
 let db = {
@@ -27,19 +56,29 @@ let db = {
             title: 'Fortinite',
             year: 2019,
             price: 00
+        }
+      ],
+      users: [
+        {
+          id: 1,
+          name: 'Cleison Carlos',
+          email: 'cleisoncarlos@hotmail.com',
+          password: 123456
         },
-    ]
+        {
+          id: 2,
+          name: 'Carlos Cleison',
+          email: 'cleisoncarlos@hotmail.com',
+          password: 654321
+        }
+      ]
 }
-
-
-
-
 
 
 
 // ednpoints
 //ver games ------------------------
-app.get('/games', (req, res)=> {
+app.get('/games', auth (req, res)=> {
     res.statusCode = 200
     res.json(db.games)
 })
@@ -125,6 +164,36 @@ app.put('/game/:id', (req, res) => {
     }
   }
 });
+
+// LOGIN
+
+
+app.post('/auth', (req, res) => {
+  let { email, password } = req.body;
+
+  if (email !== undefined) {
+    let user = db.users.find(u => u.email === email);
+
+    if (user !== undefined) {
+      if (user.password === password) {
+        jwt.sign({ id: user.id, email: user.email }, JWTsecret, { expiresIn: '24h' }, (err, token) => {
+          if (err) {
+            res.status(400).json({ err: 'Falha interna' });
+          } else {
+            res.status(200).json({ token: token });
+          }
+        });
+      } else {
+        res.status(401).json({ err: 'Credenciais inválidas' });
+      }
+    } else {
+      res.status(404).json({ err: 'Email enviado não existe na base de dados' });
+    }
+  } else {
+    res.status(400).json({ err: 'Email enviado é inválido' });
+  }
+});
+
 
   
 // fim dos endpoints-------------
